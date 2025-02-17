7e2e734dc70c27d2d5cad5b61b88c9d849541b77
Sang-Hyun-Kim <960322ksh@hanmail.net>
2025-02-17 21:28:24 +0900

    기본적인 무기 시스템과 이 를 위한 BaseItem 더미 코드 작성(Pull Request 연습에 쓰일 것)

---
+++ Team8_Project/Source/Team8_Project/BaseItem.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseItem.h"
+
+// Sets default values
+ABaseItem::ABaseItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void ABaseItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void ABaseItem::OnItemOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void ABaseItem::OnItemEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+void ABaseItem::ActivateItem(AActor* Activator)
+{
+}
+
+FName ABaseItem::GetItemType() const
+{
+	return FName();
+}
+
