72f89b1937f6d258b044e60f515e3ff4f3f4d913
balamwind <yyo0033@naver.com>
2025-02-19 19:09:11 +0900

    Fix Enemy AI, Add Enemy Attack
    플레이어를 보면 달려가서 플레이어를 때리는 기본적인 AI 제작. 아직 프로토타입입니다. \n
    IDamageabl 인자 까먹은거 추가
    몬스터의 공격 기능 추가

--- Team8_Project/Source/Team8_Project/Enemy/BaseEnemy.cpp
+++ Team8_Project/Source/Team8_Project/Enemy/BaseEnemy.cpp
@@ -1,6 +1,9 @@
 #include "BaseEnemy.h"
 
 #include "EnemyAIController.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/PlayerState.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ABaseEnemy::ABaseEnemy()
@@ -9,8 +12,6 @@ ABaseEnemy::ABaseEnemy()
 
 	AIControllerClass = AEnemyAIController::StaticClass();
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
-	
-	
 }
 
 void ABaseEnemy::BeginPlay()
@@ -18,4 +19,83 @@ void ABaseEnemy::BeginPlay()
 	Super::BeginPlay();
 
 	
+}
+
+float ABaseEnemy::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator,
+                             AActor* DamageCauser)
+{
+	float RealDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+	SetHP(HP - RealDamage);
+
+	return RealDamage;
+}
+
+bool ABaseEnemy::CanAttack()
+{
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	check(PlayerController)
+
+	ACharacter* Player = PlayerController->GetCharacter();
+	check(Player)
+
+	float Distance = FVector::Distance(Player->GetActorLocation(), GetActorLocation());
+	for (int i = 0; i < AttackPatterns.Num(); i++)
+		if (Distance <= AttackPatterns[i].AttackRange)
+			return true;
+
+	return false;
+}
+
+void ABaseEnemy::Attack()
+{
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	check(PlayerController)
+
+	ACharacter* Player = PlayerController->GetCharacter();
+	check(Player)
+
+	float Distance = FVector::Distance(Player->GetActorLocation(), GetActorLocation());
+	TArray<FAttackPattern> Patterns = AttackPatterns.FilterByPredicate([Distance](const FAttackPattern& Pattern)
+	{
+		return Distance <= Pattern.AttackRange;
+	});
+
+	if (USkeletalMeshComponent* Mesh = GetComponentByClass<USkeletalMeshComponent>())
+		if (UAnimInstance* AnimInstance = Mesh->GetAnimInstance())
+		{
+			int PatternIndex = FMath::RandRange(0, Patterns.Num() - 1);
+			AnimInstance->Montage_Play(AttackPatterns[PatternIndex].Anim);
+		}
+}
+
+bool ABaseEnemy::IsAttacking()
+{
+	if (USkeletalMeshComponent* Mesh = GetComponentByClass<USkeletalMeshComponent>())
+		if (UAnimInstance* AnimInstance = Mesh->GetAnimInstance())
+			return AnimInstance->IsAnyMontagePlaying();
+
+	return false;
+}
+
+void ABaseEnemy::OnDeath()
+{
+	if (USkeletalMeshComponent* Mesh = GetComponentByClass<USkeletalMeshComponent>())
+		if (UAnimInstance* AnimInstance = Mesh->GetAnimInstance())
+			AnimInstance->Montage_Play(DeathMontage);
+}
+
+float ABaseEnemy::GetHP() const
+{
+	return HP;
+}
+
+void ABaseEnemy::SetHP(float Value)
+{
+	HP = Value;
+
+	if (HP <= 0)
+	{
+		HP = 0;
+		OnDeath();
+	}
 }
--- Team8_Project/Source/Team8_Project/Enemy/BaseEnemy.h
+++ Team8_Project/Source/Team8_Project/Enemy/BaseEnemy.h
@@ -2,19 +2,62 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Team8_Project/Damageable.h"
 #include "BaseEnemy.generated.h"
 
-class ATargetPoint;
+class APatrolPath;
+
+USTRUCT(Atomic, BlueprintType)
+struct FAttackPattern
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UAnimMontage* Anim;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float AttackRange;
+};
 
 UCLASS()
-class TEAM8_PROJECT_API ABaseEnemy : public APawn
+class TEAM8_PROJECT_API ABaseEnemy : public APawn, public IDamageable
 {
 	GENERATED_BODY()
 
 public:
 	ABaseEnemy();
 
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator,
+	                         AActor* DamageCauser) override;
+
+	UFUNCTION(BlueprintCallable)
+	bool CanAttack();
+	
+	UFUNCTION(BlueprintCallable)
+	void Attack();
+	
+	UFUNCTION(BlueprintCallable)
+	bool IsAttacking();
+
+	virtual float GetHP() const override;
+	virtual void SetHP(float Value) override;
+
 protected:
 	virtual void BeginPlay() override;
 
+	virtual void OnDeath();
+
+protected:
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category="Attack")
+	TObjectPtr<APatrolPath> PatrolPath;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Attack")
+	TArray<FAttackPattern> AttackPatterns;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UAnimMontage> DeathMontage;
+
+private:
+	float HP;
 };
