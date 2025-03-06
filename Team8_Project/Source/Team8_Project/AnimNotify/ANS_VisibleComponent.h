b9ba6c3600be097fe5008a21b41f3d8dd244a764
balamwind <yyo0033@naver.com>
2025-03-07 00:59:51 +0900

    Add Attack Alert, Add Apear Message
    Helix 등장 시 알림
    Helix가 Attack3 공격 시 공격 범위가 보임

---
+++ Team8_Project/Source/Team8_Project/AnimNotify/ANS_VisibleComponent.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "ANS_VisibleComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM8_PROJECT_API UANS_VisibleComponent : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+public:
+	UPROPERTY(EditAnywhere)
+	FName ComponentName;
+};
